f1f658a3f61850384bb4dd966ef5c5fbb81a2754
spointu <spointu@gmail.com>
2018-04-06 23:03:04 +1200

    Release version 1.9.0

--- scripts_installer/generate-pulse-agent-win.sh
+++ scripts_installer/generate-pulse-agent-win.sh
@@ -61,7 +61,7 @@ BASE_URL="https://agents.siveo.net" # Overridden if --base-url is defined
 cd "`dirname $0`"
 
 # To be defined
-AGENT_VERSION="1.8.7"
+AGENT_VERSION="1.9.0"
 PYTHON_VERSION="2.7.9"
 PY_WIN32_VERSION="219"
 PY_NETIFACES_MODULE="netifaces"
@@ -109,7 +109,7 @@ RDPWRAP_NAME="RDPWrap"
 RDPWRAP_VERSION="1.6.1"
 DOWNLOAD_FOLDER="downloads"
 PULSE_AGENT_PLUGINS_NAME="pulse-agent-plugins"
-PULSE_AGENT_PLUGINS_VERSION="1.3"
+PULSE_AGENT_PLUGINS_VERSION="1.4"
 
 
 # Display usage
