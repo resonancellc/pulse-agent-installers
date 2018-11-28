f7ae9c42664648fa74a7a747753c29bd76e457a5
Nicolas Lécureuil <neoclust@mageia.org>
2018-11-28 10:07:07 +0100

    Copy config files for deb packaging

--- scripts_installer/lin/generate-pulse-agent-linux.sh
+++ scripts_installer/lin/generate-pulse-agent-linux.sh
@@ -108,10 +108,14 @@ create_repos() {
 
 	colored_echo green "### INFO Creating package repositories... Done"
 }
-
-
 generate_agent_installer() {
 	colored_echo blue "### INFO Generating installer..."
+
+    # We copy the config files to deb bundle
+    mkdir -p deb/pulse-agent-linux/etc/pulse-xmpp-agent
+    for config_files in $PULSE_AGENT_CONFFILE_FILENAME $PULSE_SCHEDULER_CONFFILE_FILENAME $PULSE_INVENTORY_CONFFILE_FILENAME; do
+        cp /var/lib/pulse2/clients/config/ $config_files deb/pulse-agent-linux/etc/pulse-xmpp-agent
+    done
 
 	PULSE_SERVER=`grep public_ip /etc/mmc/pulse2/package-server/package-server.ini.local | awk '{print $3}'`
 	sed -e "s/@@PULSE_SERVER@@/${PULSE_SERVER}/" install-pulse-agent-linux.sh.in > install-pulse-agent-linux.sh
