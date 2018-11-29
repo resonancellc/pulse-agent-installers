1cffa57f9671cd6777dbbc2594b79f110e2737d3
spointu <spointu@gmail.com>
2018-11-29 21:18:31 +0800

    Review dependencies for linux machine agent + options for generation of the agent

--- scripts_installer/generate-pulse-agent.sh
+++ scripts_installer/generate-pulse-agent.sh
@@ -240,9 +240,9 @@ generate_agent_lin() {
   # Generate Pulse Agent for Linux
   colored_echo blue "Generating Pulse Agent for Linux..."
 	if [ -n "${INVENTORY_TAG}" ]; then
-		COMMAND="./lin/generate-pulse-agent-linux.sh --inventory-tag=${INVENTORY_TAG}"
+		COMMAND="./lin/generate-pulse-agent-linux.sh --inventory-tag=${INVENTORY_TAG} ${OPTIONS_MINIMAL}"
 	else
-		COMMAND="./lin/generate-pulse-agent-linux.sh"
+		COMMAND="./lin/generate-pulse-agent-linux.sh ${OPTIONS_MINIMAL}"
 	fi
 	echo "Running "${COMMAND}
 	${COMMAND}
