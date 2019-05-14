3004e9348a2206029add1e9d476d88eac9228ec4
botheis <noel.kneib@siveo.net>
2019-05-14 16:43:17 +0200

    Add the GPL to the SMWinservice.py
    add GPL
    modify the service display name

--- scripts_installer/win/SMWinservice.py
+++ scripts_installer/win/SMWinservice.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+#
+# (c) 2016 siveo, http://www.siveo.net
+#
+# This file is part of Pulse 2, http://www.siveo.net
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
 import socket
 
 import win32serviceutil
@@ -11,7 +33,7 @@ class SMWinservice(win32serviceutil.ServiceFramework):
     '''Base class to create winservice in Python'''
 
     _svc_name_ = 'pythonService'
-    _svc_display_name_ = 'Toto'
+    _svc_display_name_ = 'Python Service'
     _svc_description_ = 'Python Service Description'
 
     @classmethod
